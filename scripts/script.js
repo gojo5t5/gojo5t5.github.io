74212a23e6ac20cd58068f8fdd50057abc453281
gojo satoru <hematteo@outlook.com>
2021-07-13 13:12:06 +0200

    fix flyingtext problem and make more responsive'

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 // flying text animation
+var translateBy;
 var flyingTexts = document.querySelectorAll(".fly-text");
 for (var i = 0; i < flyingTexts.length; i++) {
   var text = flyingTexts[i];
   letTextFly(text);
 }
+// var mq = window.matchMedia("(max-width: 600px)");
+// if (mq.matches) {
+//   translateBy = "translateX(-200%)";
+// } else {
+//   translateBy = "translateX(-100%)";
+// }
 function letTextFly(text) {
   text.keyframes = [
     { opacity: 0.5 },
     { opacity: 1 },
     { opacity: 0.75 },
-    { transform: "translateX(-120%)" },
+    { transform: "translateX(-100%)" },
   ];
   text.animProps = {
     duration: 5000 + Math.random() * 10000,
